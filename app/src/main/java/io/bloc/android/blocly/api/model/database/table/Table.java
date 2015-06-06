67ff9687f20a24f13414175a792e8a8b6a37f750
John McIntosh II <jmci10@hotmail.com>
2015-06-06 03:12:24 -0400

    Checkpoint 54 completed

--- app/src/main/java/io/bloc/android/blocly/api/model/database/table/Table.java
+++ app/src/main/java/io/bloc/android/blocly/api/model/database/table/Table.java
@@ -4,6 +4,11 @@ package io.bloc.android.blocly.api.model.database.table;
 import android.database.sqlite.SQLiteDatabase;
 
 public abstract class Table {
+
+    public static interface Builder {
+        public long insert(SQLiteDatabase writableDB);
+    }
+
     protected static final String COLUMN_ID = "id";
     public abstract String getName();
     public abstract String getCreateStatement();
